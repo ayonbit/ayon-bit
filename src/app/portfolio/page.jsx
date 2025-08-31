3aa48b5650fbac681c0852bd20654e6f0b70983d
Ayon <suprovatbit@gmail.com>
2025-09-01 00:15:38 +0600

    PortFolio and About SEO

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,39 +1,31 @@
 import PortfolioCard from "@/components/PortfolioCard";
-import { projectData } from "@/lib/data";
 
 //SEO
 export async function generateMetadata() {
-  const firstProject = projectData[0];
-
   return {
-    title: `Portfolio | ${firstProject.title}`,
-    description: firstProject.description,
+    title: "Portfolio | My Creative Projects",
+    description:
+      "Explore my portfolio showcasing projects in web development, design, and creative work.",
     openGraph: {
-      title: `Portfolio | ${firstProject.title}`,
-      description: firstProject.description,
+      title: "Portfolio | My Creative Projects",
+      description:
+        "Explore my portfolio showcasing projects in web development, design, and creative work.",
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/portfolio`,
       images: [
         {
-          url: firstProject.image?.startsWith("http")
-            ? firstProject.image
-            : `${process.env.NEXT_PUBLIC_SITE_URL}${
-                firstProject.image || "/images/opengraph-image.png"
-              }`,
+          url: `${process.env.NEXT_PUBLIC_SITE_URL}/images/opengraph-image.png`,
           width: 1200,
           height: 630,
-          alt: firstProject.title || "Portfolio Image",
+          alt: "Portfolio Projects",
         },
       ],
     },
     twitter: {
       card: "summary_large_image",
-      title: `Portfolio | ${firstProject.title}`,
-      description: firstProject.description,
-      image: firstProject.image?.startsWith("http")
-        ? firstProject.image
-        : `${process.env.NEXT_PUBLIC_SITE_URL}${
-            firstProject.image || "/images/opengraph-image.png"
-          }`,
+      title: "Portfolio | My Creative Projects",
+      description:
+        "Explore my portfolio showcasing projects in web development, design, and creative work.",
+      image: `${process.env.NEXT_PUBLIC_SITE_URL}/images/opengraph-image.png`,
     },
   };
 }
